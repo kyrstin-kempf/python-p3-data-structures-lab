c51aa0b3cb24bf0c2d295bd59ef520d74ce0125a
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-09-13 21:12:13 -0400

    completed data structures practice functions!

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,4 +1,3 @@
-import ipdb
 heat_icon = "🌶"
 
 spicy_foods = [
@@ -31,15 +30,19 @@ def print_spicy_foods(spicy_foods):
     for item in spicy_foods:
         print('{} ({}) | Heat Level: {}'.format(item.get("name"), item.get("cuisine"), item.get("heat_level") * heat_icon))
 
-ipdb.set_trace()
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food_dict in spicy_foods:
+        if food_dict["cuisine"] == cuisine:
+            return food_dict
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    print_spicy_foods(get_spiciest_foods(spicy_foods))
 
 def get_average_heat_level(spicy_foods):
-    pass
+    heat_levels_list = list([food_item.get("heat_level") for food_item in spicy_foods])
+    heat_level_avg = sum(heat_levels_list) / len(heat_levels_list)
+    return heat_level_avg
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
